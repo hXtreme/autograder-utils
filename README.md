e21d71318434fc522373a854a6fea364ff17d100
Harsh Parekh <harsh_parekh@outlook.com>
2022-05-17 00:24:13 -0400

    Setup basic package structure.

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # autograder-utils
-A library of python functions to make building autograder simpler.
+
+A library of python functions to make building autograders simpler.
+
+## Installation
+
+Install using pip
+
+```sh
+pip install git+https://github.com/hXtreme/autograder-utils.git
+```
+
+Install for development
+
+```sh
+git clone https://github.com/hXtreme/autograder-utils.git
+cd autograder-utils
+pip install -e .
+```
+
+Import `agu` into your grading script and you're good to go.
+
+```sh
+import agu
+```
---
+++ setup.py
@@ -0,0 +1,20 @@
+from os import path
+
+from setuptools import setup, find_packages
+
+BASE_PATH = path.dirname(path.abspath(__file__))
+
+with open(f"{BASE_PATH}/requirements.txt", "r") as fp:
+    requirements = fp.read().splitlines()
+
+setup(
+    name="autograder-utils",
+    author="Harsh Parekh",
+    author_email="harsh_parekh@outlook.com",
+    url="https://git.parekh.page/autograder-utils",
+    python_requires=">=3.7",
+    description="A library of python functions to make building autograder simpler.",
+    license="MIT",
+    packages=find_packages(include=["agu", "agu.*"]),
+    install_requires=requirements,
+)
